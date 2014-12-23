950e58e6d2986f3a9643aa2a4204d51e944bc37d
wcluijt <wcluijt@gmail.com>
2014-12-23 15:11:35 -0800

    Updated puppet module versions

--- vagrant/puppet/modules.sh
+++ vagrant/puppet/modules.sh
@@ -5,17 +5,17 @@ if [ ! -d "/etc/puppet/modules" ]; then
 fi
 
 if [ ! -d "/etc/puppet/modules/apache" ]; then
-    puppet module install -v 1.0.1 puppetlabs-apache;
+    puppet module install -v 1.2.0 puppetlabs-apache;
 fi
 
 if [ ! -d "/etc/puppet/modules/mysql" ]; then
-    puppet module install -v 2.2.3 puppetlabs-mysql;
+    puppet module install -v 3.1.0 puppetlabs-mysql;
 fi
 
 if [ ! -d "/etc/puppet/modules/apt" ]; then
-    puppet module install -v 1.4.2 puppetlabs-apt;
+    puppet module install -v 1.7.0 puppetlabs-apt;
 fi
 
 if [ ! -d "/etc/puppet/modules/git" ]; then
-    puppet module install -v 0.0.3 puppetlabs-git;
+    puppet module install -v 0.3.0 puppetlabs-git;
 fi
